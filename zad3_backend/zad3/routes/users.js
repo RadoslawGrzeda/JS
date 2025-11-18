f61da403d64f879dc9f0dd8738382e504c2ff4d3
RadosławGrzęda <radoslaw.grzeda@pjwstk.edu.pl>
2025-11-18 07:49:38 +0100

    addMiddle

--- zad3_backend/zad3/routes/users.js
+++ zad3_backend/zad3/routes/users.js
@@ -1,115 +1,84 @@
-const e = require('express');
+// const e = require('express');
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.render("users");
-  // res.send('respond with a resource');
 });
 
-router.post('/', function(req, res,next){
-  var username = req.body.Username;
-  var email = req.params.Email;
-  // validateUsername(username,"usernameError",res,req,next);
-  validateEmail(email,"emailError",res,req,next);
+function checkUsername(req,res,next){
+    const Username=req.body.Username;
+    const error = req.validationErrors || {}
 
+    if(Username.length <5 || Username.length>15)
+        error.usernameError="Username must be between 5 and 15 characters long"
 
-});
-
-
-function validateUsername(username,spanError,res,req,next){
-  const usernameValue=username;
-  if(usernameValue.length<5 || usernameValue.length>15){
-        return setError(spanError,res,'Username must be between 5 and 15 characters long.');}
-  else{
-    res.render("users",{usernameError:""});
-    next()
-    console.log(username)
-    }
-  }
-  
-  function validateEmail(email,spanError,res,req,next){
-      const emailValue=email;
-      const regex='^[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z]{2,}$';
-      // if(!emailValue.match(regex)){
-      // if(regex.matchAll(emailValue).length===0){
-      if(emailValue.length<5 || emailValue.length>50){
-      // if(!/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$/.test(emailValue)){
-      return setError(spanError,res,'Username must be between 5 and 15 characters long.');
-      }else{
-       res.render("users",{emailError:""});
-    next()
-      console.log(emailValue)
-      }
-  }
-
-
-function setError(errorEl,res,message){
-    res.render('users',{[errorEl]:message});
-    // input.classList.add("error");
-    // return false;
+    req.validationErrors=error
+    next();
 }
+function checkEmail(req,res,next){
+    const Email=req.body.Email;
+    const error = req.validationErrors || {}
+    
+    const regex='^[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z]{2,}$';
+    if(!Email.match(regex))
+        error.emailError="Email is not valid"
 
-function setSuccess(input,errorEl){1
-    console.log(input + "is success");
-    errorEl.innerText="";
-    input.classList.remove("error");
-    return true;
+    req.validationErrors=error
+    next();
 }
-
-
-function validatePassword(password){
+function validatePassword(req,res,next){
     const upper=/[A-Z]/;
     const digit=/[0-9]/;
-    const passwordValue=password.value.trim();
+    const passwordValue=req.body.Password;
+    const errors=req.validationErrors || {}
+
     if(passwordValue.length<8){
-        return setError(password,passwordError,'Password must be at least 8 characters long.');
+        errors.passwordError='Password must be at least 8 characters long.';
     }
     else if(!upper.test(passwordValue)){
-        return setError(password,passwordError,'Password must contain at least one uppercase letter.');
+    errors.passwordError='Password must contain at least one uppercase letter.';
     }else if(!digit.test(passwordValue)){
-        return setError(password,passwordError,'Password must contain at least one digit.');
-    }else {
-        return setSuccess(password,passwordError);
+    errors.passwordError='Password must contain at least one digit.';
     }
+    req.validationErrors=errors
+    next()
 }
 
-function validateAgainPassword(confirmPassword)
-{
-    const confirmPasswordValue=confirmPassword.value.trim();
-    const passwordValue=password.value.trim();
+
+function validateAgainPassword(req,res,next){
+    const confirmPasswordValue=req.body.ConfirmPassword;
+    const password=req.body.Password;
+    const errors=req.validationErrors || {}
     if (confirmPasswordValue===""){
-        return setError(confirmPassword,confirmPasswordError,'Please confirm your password.');
-    }else {
-    if(confirmPasswordValue !==passwordValue){
-        return setError(confirmPassword,confirmPasswordError,'Passwords do not match.');
+    errors.passwordConfirmError='Please confirm your password.';
+    }else if(confirmPasswordValue !==password){
+    errors.passwordConfirmError='Passwords do not match.';
     }
-    else{
-        return setSuccess(confirmPassword,confirmPasswordError);
+    req.validationErrors=errors
+    next();
+    
 }
- }}
 
-// function setError(input,errorEl,message){
-//     errorEl.innerText=message;
-//     input.classList.add("error");
-//     return false;
-// }
+function endWithRender(req,res,next){
+    const errors = req.validationErrors || {}
+    if(Object.keys(errors).length > 0){
+        return res.render('users',errors)
+    }
+    next()
+} 
 
-// function setSuccess(input,errorEl){1
-//     console.log(input + "is success");
-//     errorEl.innerText="";
-//     input.classList.remove("error");
-//     return true;
-// }
+router.post('/',
+            checkUsername,
+            checkEmail,
+            validatePassword,
+            validateAgainPassword,
+            endWithRender,
+            function(req, res,next){
 
-// function clearErrors(){
-//     usernameError.innerText="";
-//     emailError.innerText="";
-//     passwordError.innerText="";
-//     confirmPasswordError.innerText="";
-// }
+res.render('users',{successMessage:"Registration successful!"})
 
-
+});
 
 module.exports = router;
